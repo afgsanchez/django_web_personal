8634bedbeafe1f7e594376f512027e135ddb435f
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2025-04-26 20:21:52 +0200

    updates

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.Model):
+class Project(models.Model): # pylint: disable=missing-class-docstring
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripción")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Link",null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    
 
     class Meta:
         verbose_name = "proyecto"
         verbose_name_plural = "proyectos"
         ordering = ["-created"]
 
-    def __str__(self):
+    def __str__(self): # pylint: disable=invalid-str-returned
         return self.title
-
